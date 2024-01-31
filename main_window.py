f76b4d9cf32da1a717361f07dabc23da5d1103d0
Ella Ksenofontova <ellaksenofontova@gmail.com>
2024-01-31 21:32:02 +0300

    Импортировала корреляционную функцию. Пока она выводит массив в консоль

--- main_window.py
+++ main_window.py
@@ -9,7 +9,14 @@ from PyQt6.QtWidgets import QApplication, \
     QHBoxLayout
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
+import ctypes
+from random import uniform
+from math import ceil
+from threading import Timer
 
+lib = ctypes.CDLL("./Kfunc.dll")
+lib.K.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.c_double, ctypes.c_double]
+lib.K.restype = ctypes.POINTER(ctypes.c_double)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,6 +29,9 @@ class MainWindow(QMainWindow):
             "Полипропилен": 1430,
             "Поливинилхлорид": 2395
         }
+
+        self.readings_1 = None
+        self.readings_2 = None
         
         self.central_widget_layout = QGridLayout()
         self.central_widget_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
@@ -62,11 +72,13 @@ class MainWindow(QMainWindow):
         self.central_widget_layout.addWidget(label, 0, 0, 0, 2)
 
         calculation_button = QPushButton("Расчёт")
+        calculation_button.setObjectName("calculationButton")
         calculation_button.setMaximumSize(100, 100)
         calculation_button.setStyleSheet("max-height: 20px;margin-top: 3px; background-color: navy; "
                                          "border: 1px solid midnigthblue; "
                                          "color: white; font-weight: bold; font-size:14px")
         self.central_widget_layout.addWidget(calculation_button, 5, 0)
+        calculation_button.clicked.connect(self.show_values_of_func)
 
         self.add_labels()
         self.add_spinboxes_and_combobox()
@@ -81,8 +93,33 @@ class MainWindow(QMainWindow):
 
             self.central_widget_layout.addWidget(label, i + 6, 0)
 
+    def show_values_of_func(self):
+        distance = self.central_widget.findChild(QDoubleSpinBox, "distanceSpinBox").value()
+        sound_speed = self.central_widget.findChild(QDoubleSpinBox, "soundSpeedSpinBox").value()
+
+        if distance != 0:
+
+            array_length = ceil(sound_speed / distance * 35000)
+
+            values = [uniform(1.0, 5.0) for i in range(array_length)]
+            self.readings_1 = self.readings_2 = (ctypes.c_double * array_length)(*values)
+
+            calculation_button = self.central_widget.findChild(QPushButton, "calculationButton")
+            calculation_button.setText("Загрузка...")
+
+            timer = Timer(0.1, self.perform_slow_calculation, [distance, sound_speed])
+            timer.start()
+
+    def perform_slow_calculation(self, distance, sound_speed):
+        array_length = ceil(distance / sound_speed * 35000)
+        result_ptr = lib.K(self.readings_1, self.readings_2, distance, sound_speed)
+        result_array = ctypes.cast(result_ptr, ctypes.POINTER(ctypes.c_double * array_length)).contents
+        print(list(result_array))
+        calculation_button = self.central_widget.findChild(QPushButton, "calculationButton")
+        calculation_button.setText("Расчёт")
+
     def add_labels(self):
-        labels_texts = ["Скорость звука, м/с:", "Расстояние между датчиками:", "Диаметр трубы:", "Материал трубы:"]
+        labels_texts = ["Скорость звука, м/с:", "Расстояние между датчиками, м:", "Диаметр трубы:", "Материал трубы:"]
 
         for i in range(len(labels_texts)):
             label = QLabel(labels_texts[i])
